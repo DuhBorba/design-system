863bb340f4b81f2e6728c15432d4d425a6cd2a0c
Eduardo Luis Borba <eduardoborba3@gmail.com>
2023-10-23 16:11:46 -0300

    Create Text Component

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel officiis velit obcaecati sit. Accusamus quo consequuntur dolores enim vero, nihil officiis, nulla natus nisi cumque nostrum inventore est, maiores minima.',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
