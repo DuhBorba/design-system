4b679743f995786e61af5be402281f05510dbcff
Eduardo Luis Borba <eduardoborba3@gmail.com>
2023-10-23 17:58:27 -0300

    Create Avatar Component

--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -4,6 +4,7 @@ import { Text, TextProps } from '@ignite-ui/react'
 export default {
   title: 'Typography/Text',
   component: Text,
+  tags: ['autodocs'],
   args: {
     children:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel officiis velit obcaecati sit. Accusamus quo consequuntur dolores enim vero, nihil officiis, nulla natus nisi cumque nostrum inventore est, maiores minima.',
