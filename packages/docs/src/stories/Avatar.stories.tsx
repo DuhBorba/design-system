e06e432cc549e8743db048d5697d3a4d7c7ce2b4
Eduardo Luis Borba <eduardoborba3@gmail.com>
2023-10-25 10:02:20 -0300

    Component Controls

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -9,6 +9,13 @@ export default {
     src: 'https://github.com/duhborba.png',
     alt: 'Eduardo Luis Borba',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
